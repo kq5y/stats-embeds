7861cbe74336fa2504d7f9d8ede97e1da1e4a72f
kq5y <contact@kq5.jp>
2025-05-22 16:42:14 +0900

    add: top & recent handlers

--- src/routes/embed/recent.ts
+++ src/routes/embed/recent.ts
@@ -1,7 +1,32 @@
 import type { Handler } from "hono";
 
+import View from "@/components/View";
+import { getApi, getRecentTracks } from "@/libraries/stats";
+
 const handler: Handler<Env, "recent"> = async (c) => {
-  return c.json({test: 0});
+  const { user } = c.req.query();
+
+  if (!user) {
+    return c.text("User not found", 400);
+  }
+
+  const api = getApi();
+  const tracks = await getRecentTracks(api, user);
+
+  const response = await c.html(
+    View({
+      title: `Recently Played by ${user}`,
+      type: "recently",
+      tracks
+    }),
+    200,
+    {
+      "Content-Security-Policy": "frame-ancestors *",
+    }
+  );
+  response.headers.delete("x-frame-options");
+
+  return response;
 }
 
 export { handler };
--- src/routes/embed/top.ts
+++ src/routes/embed/top.ts
@@ -1,7 +1,37 @@
 import type { Handler } from "hono";
 
+import View from "@/components/View";
+import { formatRange, getApi, getTopTracks, isRange } from "@/libraries/stats";
+
 const handler: Handler<Env, "top"> = async (c) => {
-  return c.json({test: 0});
+  const { user, range = "weeks" } = c.req.query();
+
+  if (!user) {
+    return c.text("User not found", 400);
+  }
+
+  if (!isRange(range)) {
+    return c.text("Invalid range", 400);
+  }
+  const formattedRange = formatRange(range);
+
+  const api = getApi();
+  const tracks = await getTopTracks(api, user, formattedRange);
+
+  const response = await c.html(
+    View({
+      title: `Top Tracks by ${user} (${formattedRange})`,
+      type: "frequently",
+      tracks
+    }),
+    200,
+    {
+      "Content-Security-Policy": "frame-ancestors *",
+    }
+  );
+  response.headers.delete("x-frame-options");
+
+  return response;
 }
 
 export { handler };
